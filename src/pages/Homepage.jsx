301417a59d2f2e1115765073dda169ab9939e1a7
SimoneCelli80 <158849846+SimoneCelli80@users.noreply.github.com>
2024-09-18 21:24:14 +0200

    Implemented brightness change on hover

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,7 +9,7 @@ const Homepage = () => {
   return (
     <div className="bg-gradient-to-r from-smoothWhite to-smoothB w-screen h-screen flex justify-center items-center select-none">
       <div className="grid grid-cols-12 grid-rows-8 h-full w-full gap-4 p-8"> 
-        <div className="col-span-7 row-span-5 bg-smoothGrey rounded-3xl p-6 shadow-2xl flex flex-col">
+        <div className="col-span-7 row-span-5 bg-smoothGrey rounded-3xl p-6 shadow-2xl flex flex-col cursor-pointer hover:brightness-110 transition duration-300">
           <h1 className="text-center font-monoton text-6xl text-smoothYellow pt-4">
             SoSmooth<span className="ml-5">OCP</span>
           </h1>
@@ -18,26 +18,26 @@ const Homepage = () => {
           </div>
         </div>
 
-        <a href="/login" className="col-span-2 row-span-1 rounded-3xl flex items-center justify-center bg-smoothGrey text-gray-200 shadow-2xl">
+        <a href="/login" className="col-span-2 row-span-1 rounded-3xl flex items-center justify-center bg-smoothGrey text-gray-200 shadow-2xl hover:brightness-110 transition duration-300">
           <h1 className="text-2xl text-smoothWhite font-bold font-poppins">
             Login
           </h1>
         </a>
 
-        <a href="/registration" className="col-span-2 row-span-1 rounded-3xl bg-smoothYellow rounded-5xl flex items-center justify-center shadow-2xl">
+        <a href="/registration" className="col-span-2 row-span-1 rounded-3xl bg-smoothYellow rounded-5xl flex items-center justify-center shadow-2xl hover:brightness-110 transition duration-300">
           <span className="text-smoothGrey text-2xl font-bold font-poppins">
             Registration
           </span>
         </a>
 
-        <div className="col-span-1 row-span-1 border-4 border-smoothGrey bg-transparent rounded-3xl shadow-2xl flex items-center justify-center cursor-pointer">
+        <div className="col-span-1 row-span-1 border-4 border-smoothGrey bg-transparent rounded-3xl shadow-2xl flex items-center justify-center cursor-pointer hover:brightness-90 transition duration-300">
           <img src={Hamburger} alt="Hamburger Menu" className="" />
         
         </div>
         <div className="bg-cover bg-center col-span-5 row-span-7 rounded-3xl shadow-2xl" style={{ backgroundImage: `url(${smooth})` }}>
         </div>
 
-        <div className=" bg-smoothWhite col-span-4 row-span-3 rounded-3xl shadow-2xl font-poppins cursor-pointer shadow-pink-100 border-1 border-pink-100">
+        <div className=" bg-smoothWhite col-span-4 row-span-3 rounded-3xl shadow-2xl font-poppins cursor-pointer shadow-pink-100 border-1 border-pink-100 hover:brightness-90 transition duration-300">
           <h2 className="text-3xl text-smoothGrey font-bold pt-6 pl-16">
             Katas
           </h2>
@@ -47,11 +47,8 @@ const Homepage = () => {
               </div>
             </div>
         </div>
-
-
-
         <div className="flex items-center justify-center col-span-3 row-span-3">
-          <div className="flex items-center justify-center w-4/5 h-4/5 bg-smoothYellow shadow-2xl  rounded-3xl font-poppins cursor-pointer">
+          <div className="flex items-center justify-center w-4/5 h-4/5 bg-smoothYellow shadow-2xl  rounded-3xl font-poppins cursor-pointer hover:brightness-110 transition duration-300">
             <h2 className="text-3xl text-smoothGrey font-bold p-5">
               Flashcards
             </h2>
